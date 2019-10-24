5bdc4ccc0fed729f10d09f4c3f482597db3939d6
xzm <speme_x@126.com>
2019-10-24 13:49:46 +0800

    颜色配置

--- pages/views/Main.vue
+++ pages/views/Main.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
     <!-- 布局组件 -->
-    <ea-layout>
+    <ea-layout BackgroundColor="#00665D">
       <!-- logo可自定义 加slot='logo'属性 -->
       <div slot="logo" class="logo">
         <img class="logo_img" src="../assets/logo.png" />
         <span>EasyAdmin</span>
       </div>
       <!-- 菜单组件 加slot='sidemenu'属性-->
-      <ea-sidemenu slot="sidemenu" />
+      <ea-sidemenu  BackgroundColor="#00665D"  TextColor="white" ActiveTextColor="#FFC057" slot="sidemenu" />
          <!-- 头部组件 加slot='header'属性-->
       <ea-header slot="header">
         <!-- 页签组件 -->
-        <ea-tagview slot="tagview" />
+        <ea-tagview  ActiveBg="#00665D" slot="tagview" />
         <!-- 右侧操作按钮，如不需要某些功能，删除其标签即可。如有自定义内容，加slot="options"属性  -->
         <!-- 全局搜索功能 -->
         <ea-search slot="options" />
@@ -36,7 +36,7 @@ export default {
 
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 .logo {
   height: 60px;
   color: #fff;
@@ -46,5 +46,9 @@ export default {
 }
 .logo_img {
   height: 60px;
+}
+// 右侧图标颜色
+.ea_header_options{
+    color: #888;
 }
 </style>
